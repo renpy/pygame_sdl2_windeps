cac09f24831140f8a37c10e5d229e14aa3b4156c
Tom Rothamel <tom@rothamel.us>
2015-12-13 14:10:01 -0500

    Batch file to create all import libraries.

---
+++ makelib.bat
@@ -0,0 +1,6 @@
+python makelib.py lib/x86/libpng16-16.dll lib/x86/png.lib x86
+python makelib.py lib/x86/libjpeg-9.dll lib/x86/jpeg.lib x86
+
+python makelib.py lib/x64/libpng16-16.dll lib/x64/png.lib x64
+python makelib.py lib/x64/libjpeg-9.dll lib/x64/jpeg.lib x64
+
